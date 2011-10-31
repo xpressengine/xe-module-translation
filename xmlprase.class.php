c4c8947d3d821600f6ea6e94ed5d9633ce05271b
emerald.zhangj <emerald.zhangj@gmail.com>
2011-10-31 05:23:56 +0000

    git-svn-id: http://xe-translation.googlecode.com/svn/trunk@5 a373423d-1a5b-309d-8937-70901df1a079

--- xmlprase.class.php
+++ xmlprase.class.php
@@ -6,8 +6,10 @@ class XMLContext {
 	private $_fileType;
 	private $_xml;
 	private $_rootName;
+	private $_defaultLang;
+	private $_file;
 
-	function __construct($file){
+	function __construct($file , $defaultLang){
 		if(!file_exists($file)){
 			//throw new Exception ("file don't exist!");
 			return;
@@ -16,10 +18,13 @@ class XMLContext {
 			//throw new Exception ("xml file error!");
 			return;
 		}
-		$this->_xml = simplexml_load_file($file);
+		$this->_file = file_get_contents($file);
+		$this->_file = preg_replace('/xml:lang/i','xml_lang',$this->_file);
+
+		$this->_defaultLang = $defaultLang;
+		$this->_xml = simplexml_load_string($this->_file);
 		$this->_fileType = $this->_getFileType();
 		$this->_xmlContext = $this->_praseFile($this->_xml, $this->_rootName);
-var_dump($this->_xmlContext);
 	}
 
 	private function _setRootName($file){
@@ -35,79 +40,83 @@ var_dump($this->_xmlContext);
 		return $this->_xmlContext;
 	}
 
-	private function _getNodeInfo($xmlObj,$rootName,&$xpath){
-		if($xmlObj instanceof SimpleXMLElement){
-			$data['attr'] = $xmlObj->attributes;
-			$data['value'] = $xmlObj;
-			foreach($xmlObj->attributes as $key => $value){
-				if(preg_match("/xml:lang=\"([^\"].+)\"/i")){
-					$data['isTransLine'] = true;
-				}
-			}
+	private function _hasSubNode($xmlObj){
+		if(!$xmlObj instanceof SimpleXMLElement){
+			return false;
 		}
-		else{
-			$data['attr'] = array();
-			$data['value'] = $xmlObj;
+		$hasChildren = false;
+		foreach($xmlObj->children() as $key => $value){
+			$hasChildren = true;
+			break;
 		}
-		$xpath = !empty($xpath) ? $xpath.'_'.$rootName:$rootName;
-		$data['xpath'] = $xpath;
+		return $hasChildren;
+	}
 
+	private function _storageNode($xmlObj, $xpath, $isLangLine){
+		$data['attr'] = $xmlObj->attributes();
+		$data['xpath'] = $xpath;
+		if($isLangLine){
+			$data['isLangLine'] = true;
+			$data['value'] = strval($xmlObj);
+			return $data;
+		}
+		$arr = explode('_',$xpath);
+		$nodeName = array_pop($arr);
+		$hasChildren = $this->_hasSubNode($xmlObj);
+
+		if(!$hasChildren){
+			$data['value'] = strval($xmlObj);
+		}else{
+			$data['value'] = $xmlObj;
+		}
 		return $data;
 	}
 
 	/*
-	*	prase the xml file to the construct
+	*	prase the xml file to find  translation lang line info
 	*
 	*/
-	private function _praseFile($xmlObj,$rootName){
+	private function _praseFile($xmlObj, $rootName){
 		static $xpath;
 		$data = array();
-		if($xmlObj instanceof SimpleXMLElement){
-
-			$id = count($data);
-			$data[$id] = $this->_getNodeInfo($xmlObj, $rootName, $xpath);
-
-
-			$nodeArr = get_object_vars($xmlObj);
-			foreach($nodeArr as $nodeName => $subNode){
-				if($nodeName == '@attributes'){
-					continue;
-				}
-				$data = array_merge($data,$this->_praseFile($subNode, $nodeName));
-				$xpathArr = explode('_',$xpath);
-				array_pop($xpathArr);
-				$xpath = implode('_',$xpathArr);
+		$xpath = !empty($xpath) ? $xpath.'_'.$rootName:$rootName;
+		$isLangLine = false;
+		foreach($xmlObj->attributes() as $atrrName => $atrrValue){
+			if($atrrName == 'xml_lang' && strval($atrrValue) == $this->_defaultLang){
+				$isLangLine = true;
 			}
-			return $data;
 		}
-		if(is_array($xmlObj)){
-			foreach($xmlObj as $index => $value){
-				$id = count($data);
-				$data[$id] = $this->_getNodeInfo($xmlObj, $index, $xpath);
-				$data[$id]['value'] = $xmlObj;
-				$data = array_merge($data, $this->_praseFile($value, $rootName.'_'.$index));
-				$xpathArr = explode('_',$xpath);
-				array_pop($xpathArr);
-				array_pop($xpathArr);
-				$xpath = implode('_',$xpathArr);
-			}
-			return $data;
+		array_push($data, $this->_storageNode($xmlObj, $xpath, $isLangLine));
+
+		foreach($xmlObj as $nodeName => $subNode){
+			$data = array_merge($data, $this->_praseFile($subNode, $nodeName));
 		}
+		$xpathArr = explode('_', $xpath);
+		array_pop($xpathArr);
+		$xpath = implode('_',$xpathArr);
 		return $data;
 	}
+
 	private function _startTag($tagInfoArr){
-		$nodeName = array_pop(explode('_', $tagInfoArr['xpath']));
+		$arr = explode('_', $tagInfoArr['xpath']);
+		$nodeName = array_pop($arr);
 		$output = sprintf('<%s',$nodeName);
-		foreach($tagInfoArr['attr'] as $avalue){
-			$output .= sprintf(' %s ' , $avalue);
+		foreach($tagInfoArr['attr'] as $aName => $avalue){
+			if($aName == "xml_lang"){
+				$aName = 'xml:lang';
+			}
+			$output .= sprintf(' %s="%s" ' , $aName, $avalue);
 		}
-		$output .= sprintf('" >');
+		$output .= sprintf('>');
 		return $output;
 	}
 
 	private function _fillContent($content){
-		$output = sprintf("%s",$content);
-		return $output;
+
+		if($content instanceof SimpleXMLElement){
+			return '';
+		}
+		return '<![CDATA['.$content.']]>';
 	}
 
 	private function _endTag($tagName){
@@ -115,29 +124,52 @@ var_dump($this->_xmlContext);
 		return $output;
 	}
 
-	public function getXmlFile($valueArray){
-		$context = $this->_xmlContext;
-		$nodeNameLine = array();
+	private function _addLangLine($parseArr, $valueArr){
+		$output = '';
+		foreach($valueArr as $vkey => $vArr){
+			if($vArr['content_node'] == $parseArr['attr']){
+				$output .= $this->_startTag($parseArr);
+				$output .= $this->_fillContent($vArr['content']);
+				$output .= $this->_endTag($nodeName);
+			}
+		}
+		return $output;
+	}
+
+	public function getXmlFile($valueArr = array(),$context = null){
+		$stackNodes = array();
+		if($context === null){
+			$context = $this->_xmlContext;
+		}
 		$output = '<?xml version="1.0" encoding="UTF-8"?>';
 		foreach($context as $key => $parseArr){
-			$nodeName = array_pop(explode('_', $parseArr['xpath']));
-			$nodeNameLine = array_push($nodeNameLine , $nodeName);
-			$output .= $this->_startTag($parseArr);
-			$output .= $this->_fillContent($parseArr['content']);
-			while(count(explode('_',$lastPath)) >= count(explode('_',$parseArr['xpath']))){
-				$output .= $this->_endTag(array_pop($nodeNameLine));
-				$lastPath = $parseArr['xpath'];
-			}
-			if(isset($parseArr['isTransLine'])){
-				foreach($valueArray as $vkey => $vArr){
-					if($vArr['content_node'] == $parseArr['attr']){
-						$output .= $this->_startTag($parseArr);
-						$output .= $this->_fillContent($vArr['content']);
-						$output .= $this->_endTag($nodeName);
-					}
-				}
+			$curXpath = $parseArr['xpath'];
+			$arr = explode('_', $curXpath);
+			$nodeName = array_pop($arr);
+
+			while(count($stackNodes) >= count(explode('_',$curXpath))){
+				$nN = array_pop($stackNodes);
+				$output .= $this->_endTag($nN);
 			}
 
+			$output .= $this->_startTag($parseArr);
+			$output .= $this->_fillContent($parseArr['value']);
+
+			if($this->_hasSubNode($parseArr['value'])){
+				$stackNodes[] = $nodeName;
+			}else{
+				$output .= $this->_endTag($nodeName);
+			}
+
+			if(isset($parseArr['isTransLine'])){
+				$output .= $this->_addLangLine($parseArr,$valueArr);
+			}
+			$lastPath = $curXpath;
+		}
+
+		while(count($stackNodes) > 0){
+			$nN = array_pop($stackNodes);
+			$output .= $this->_endTag($nN);
 		}
 		return $output;
 	}
@@ -157,29 +189,4 @@ var_dump($this->_xmlContext);
 
 		return $this->_fileType;
 	}
-
-	private function _nodeIterator($object){
-	   $return = NULL;
-
-	   if(is_array($object))
-	   {
-	       foreach($object as $key => $value)
-	           $return[$key] = object2array($value);
-	   }
-	   else
-	   {
-	       $var = get_object_vars($object);
-
-	       if($var)
-	       {
-	           foreach($var as $key => $value)
-	               $return[$key] = object2array($value);
-	       }
-	       else
-	           return $object;
-	   }
-
-	   return $return;
-	}
-
 }
