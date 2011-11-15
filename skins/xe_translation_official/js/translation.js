88633cc009edda321294e0abce579fea226c7a42
unryong <unryong@gmail.com>
2011-11-15 10:36:19 +0000

    git-svn-id: http://xe-translation.googlecode.com/svn/trunk@56 a373423d-1a5b-309d-8937-70901df1a079

--- skins/xe_translation_official/js/translation.js
+++ skins/xe_translation_official/js/translation.js
@@ -1,20 +1,56 @@
 jQuery(function($){
-	var eArr = [];
-	var track,w;
-	eArr = $('tr .track>span');
 	
+	var eArr = [];
+	var tracks,sp,tw,sw;
+	//eArr = $('tr .track>span');
+	tracks = $('tr div.track');
+	tw = $(".track").css("width").replace(/px$/,'') - 1;
+	var i = 0;
+	while(i < tracks.length){
+		sp = $(tracks[i]).find('span');
+		
+		var m = 0;
+		while(m<sp.length){
+			var el = $(sp[m]);
+			w = tw*(el.html().replace(/%$/,'')*.01);
+			if (m == 0 || m == (sp.length - 1)){
+				(w < 5)? w = (5 + 'px'):w = (w + 'px'); 
+				el.css('text-indent','-1000');
+			} else{
+				w = (w - 10) + 'px';
+			}
+			el.css('width',w);
+			m++;
+		}
+		i++;
+	} // End of while [i]
+	
+	
+	/*
   	var i = 0;
 	track = $(".track").css("width").replace(/px$/,'');
 	//alert($(".percentage").css("width"));
 	track = track - 1;
+	alert(eArr.length);
 	while (i < eArr.length){
-		w = track*($(eArr[i]).html().replace(/%$/,'')*.01) + 'px';
-		$(eArr[i]).css('width',w);
-
+		w = track*($(eArr[i]).html().replace(/%$/,'')*.01)
+		if(w < 1 ){
+			w = 3 + 'px';
+			$(eArr[i]).css('width',w);
+		}else{
+			w = w + 'px';
+			$(eArr[i]).css('width',w);
+		}
 		
 		i++;
 		
 	}
+	*/
+	
+	
+	
+	
+	
 	
 	$('tbody tr:last-child>td,tbody tr:last-child>th').css('border-bottom','none');
 	$('thead th:last-child').css('border-right','none');
