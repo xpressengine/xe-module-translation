942b564a9304e557ed3cd7882896297985ff4fd0
unryong <unryong@gmail.com>
2011-11-18 09:31:03 +0000

    git-svn-id: http://xe-translation.googlecode.com/svn/trunk@96 a373423d-1a5b-309d-8937-70901df1a079

--- skins/xe_translation_official/js/script.js
+++ skins/xe_translation_official/js/script.js
@@ -1,4 +1,51 @@
 jQuery(function($){
+	// select language comboBox
+	var combo = $('div.lang_select');
+	var list_li = combo.find('ul.lang_list>li');
+
+	var wpx = combo.find('ul.lang_list').css('width');
+	var w = Number(wpx.replace(/px$/,''));
+	combo.find('>div').css('width',(w+3)+'px');
+	combo.find('a').css({'display':'inline-block','width':(w-15) + 'px'});
+	
+	//combo.find('ul.lang_list').css('display','none');
+	list_li.mouseover(function(){$(this).css('background-color','#dedede')});
+	list_li.mouseout(function(){$(this).css('background-color','transparent')});
+	var combo_tf = $('div.lang_select>div');
+	combo.click(listToggle).focusout(leave);
+	combo.find('a').click(change_lang);
+	function change_lang()
+	{
+		var lnk = $(this);
+		var p = lnk.parents('.lang_select')
+		var field = p.find('div.selected>span.label');
+		field.html(lnk.html());
+		var banner = p.find('div.selected>span.ico');
+		banner.removeClass().addClass(lnk.attr('class') + ' ico');
+	}
+
+	function listToggle()
+	{
+		var sel = $(this);
+		
+		var state;
+		($('div.lang_select>div').hasClass('on'))?state = true:state = false;
+		
+		if (state){
+			sel.find('ul').css('display','none');
+			sel.find('div.selected').removeClass('on').addClass('off');
+		}else{
+			sel.find('ul').css('display','block');
+			
+			sel.find('div.selected').removeClass('off').addClass('on');
+		}
+	}
+
+	function leave()
+	{
+		combo.find('ul').css('display','none');
+		combo_tf.removeClass('on').addClass('off');
+	}
 
 	var _root = this;
 	_root.targetLang = targetLang;
