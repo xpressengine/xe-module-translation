2cc361f0e348637da586905fd9d404eee8bdd55d
xe.master153 <xe.master153@gmail.com>
2011-11-21 03:04:45 +0000

    git-svn-id: http://xe-translation.googlecode.com/svn/trunk@101 a373423d-1a5b-309d-8937-70901df1a079

--- skins/xe_translation_official/js/translation.js
+++ skins/xe_translation_official/js/translation.js
@@ -4,59 +4,32 @@ jQuery(function($){
 	var tracks,sp,tw,sw;
 	//eArr = $('tr .track>span');
 	tracks = $('tr div.track');
+	
 	tw = $(".track").css("width").replace(/px$/,'') - 1;
+
 	var i = 0;
 	while(i < tracks.length){
 		sp = $(tracks[i]).find('span');
-		
 		var m = 0;
-		//var flag = 0;
-		
+		var w_add = 0;
 		while(m<sp.length){
 			var el = $(sp[m]);
-			var isFe = (m == 0);
-			var isLe = (m == (sp.length - 1));
 			w = tw*(el.html().replace(/%$/,'')*.01);
-			if (foe){
-				(w < 5)? w = (5 + 'px'):w = (w + 'px'); 
-				el.css('text-indent','-1000');
-				flag = true;
-			} 
-			if(flag && !foe){
-				w = (w - 10) + 'px';
+			if( w < 10){
+				w_add = 10 - w;
+				w = (10 + 'px');
+			}else{
+				if(w_add > 0){
+					w = w - w_add;
+					w_add = 0;
+				}
+				w = (w + 'px'); 
 			}
 			el.css('width',w);
 			m++;
 		}
 		i++;
 	} // End of while [i]
-	
-	
-	/*
-  	var i = 0;
-	track = $(".track").css("width").replace(/px$/,'');
-	//alert($(".percentage").css("width"));
-	track = track - 1;
-	alert(eArr.length);
-	while (i < eArr.length){
-		w = track*($(eArr[i]).html().replace(/%$/,'')*.01)
-		if(w < 1 ){
-			w = 3 + 'px';
-			$(eArr[i]).css('width',w);
-		}else{
-			w = w + 'px';
-			$(eArr[i]).css('width',w);
-		}
-		
-		i++;
-		
-	}
-	*/
-	
-	
-	
-	
-	
 	
 	$('tbody tr:last-child>td,tbody tr:last-child>th').css('border-bottom','none');
 	$('thead th:last-child').css('border-right','none');
