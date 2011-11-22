584a389df2f69ae7b00955a34132c42365eb7c58
xe.master153 <xe.master153@gmail.com>
2011-11-22 06:13:27 +0000

    git-svn-id: http://xe-translation.googlecode.com/svn/trunk@119 a373423d-1a5b-309d-8937-70901df1a079

--- skins/xe_translation_official/js/translation.js
+++ skins/xe_translation_official/js/translation.js
@@ -3,34 +3,37 @@ jQuery(function($){
 	var eArr = [];
 	var tracks,sp,tw,sw;
 	//eArr = $('tr .track>span');
-	tracks = $('tr div.track');
-	
-	tw = $(".track").css("width").replace(/px$/,'') - 1;
 
-	var i = 0;
-	while(i < tracks.length){
-		sp = $(tracks[i]).find('span');
-		var m = 0;
-		var w_add = 0;
-		while(m<sp.length){
-			var el = $(sp[m]);
-			w = tw*(el.html().replace(/%$/,'')*.01);
-			if( w < 10){
-				w_add = 10 - w;
-				w = (10 + 'px');
-			}else{
-				if(w_add > 0){
-					w = w - w_add;
-					w_add = 0;
+	tracks = $('tr div.track');
+
+	if(tracks.length>0){
+		tw = $(".track").css("width").replace(/px$/,'') - 1;
+
+		var i = 0;
+		while(i < tracks.length){
+			sp = $(tracks[i]).find('span');
+			var m = 0;
+			var w_add = 0;
+			while(m<sp.length){
+				var el = $(sp[m]);
+				w = tw*(el.html().replace(/%$/,'')*.01);
+				if( w < 10){
+					w_add = 10 - w;
+					w = (10 + 'px');
+				}else{
+					if(w_add > 0){
+						w = w - w_add;
+						w_add = 0;
+					}
+					w = (w + 'px'); 
 				}
-				w = (w + 'px'); 
+				el.css('width',w);
+				m++;
 			}
-			el.css('width',w);
-			m++;
-		}
-		i++;
-	} // End of while [i]
-	
-	$('tbody tr:last-child>td,tbody tr:last-child>th').css('border-bottom','none');
-	$('thead th:last-child').css('border-right','none');
+			i++;
+		} // End of while [i]
+		
+		$('tbody tr:last-child>td,tbody tr:last-child>th').css('border-bottom','none');
+		$('thead th:last-child').css('border-right','none');
+	}
  });
